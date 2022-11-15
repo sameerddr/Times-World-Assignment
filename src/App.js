8975f805df9c965438593c1ea92f7c915421ebed
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-15 12:00:31 +0530

    Assigment Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./Components/Header/Nav";
+import Home from "./Components/Header/Home/Home";
+import Login from "./Components/Header/Login/Login";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/" element={<Login />} end />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/Header/Home/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import "./Home.css";
+
+function Home({ set }) {
+  const navigate = useNavigate();
+
+  const [data, setdata] = useState([]);
+
+  const api = "https://restcountries.com/v2/all?fields=name,region,flag";
+
+  const getapi = async () => {
+    const response = await axios.get(api);
+    setdata(response.data);
+  };
+
+  useEffect(() => {
+    getapi();
+  }, []);
+
+  const handlelogout = () => {
+    set(false);
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="main">
+        <div className="data1">
+          <h1> Welcome to the Data Center</h1>
+          <button onClick={handlelogout} className="logout">
+            Log Out
+          </button>
+        </div>
+        {data?.map((item) => (
+          <div className="data" key={item.name}>
+            <div className="container">
+              <img
+                src={item.flag}
+                alt="Error-Loading-Image"
+                className="image"
+              />
+              <div className="detail">
+                <h6>{item.name}</h6>
+                <h5>{item.region}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Home;
