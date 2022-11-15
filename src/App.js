8e733aadc89772a9de0ceb06673aa23aa8a30dae
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-15 14:08:03 +0530

    Output Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./Components/Header/Nav";
-import Home from "./Components/Header/Home/Home";
 import Login from "./Components/Header/Login/Login";
 
 import "./App.css";
@@ -9,7 +7,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Nav />
         <Routes>
           <Route path="/" element={<Login />} end />
         </Routes>
