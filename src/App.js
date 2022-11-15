8b9d8ab3245264e5a895e2cbd6e61369f77865cb
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-15 13:35:59 +0530

    Responsiveness Added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
-          {/* <Route path="/home" element={<Home />} /> */}
           <Route path="/" element={<Login />} end />
         </Routes>
       </BrowserRouter>
